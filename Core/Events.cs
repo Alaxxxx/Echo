3f2736d62e0e46c43832e12a27a65533f09e2c8c
Alaxxxx <mori.alexei@outlook.fr>
2025-08-04 11:23:30 +0200

    Remove filtered event subscription logic
    Eliminated support for filtered event subscriptions from EventBus and Events classes, including related methods and data structures. This simplifies event handling by removing the ability to subscribe with filters.

--- Core/Events.cs
+++ Core/Events.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Runtime.CompilerServices;
 using Echo.Interface;
 
@@ -8,45 +7,6 @@ namespace Echo.Core
       internal static class Events<T> where T : struct, IEvent
       {
             internal static event Action<T> OnEvent;
-
-            private readonly static List<Subscriber> _filteredSubscribers = new();
-
-            private readonly struct Subscriber
-            {
-                  public readonly Action<T> Action { get; }
-                  public readonly Func<T, bool> Filter { get; }
-
-                  public Subscriber(Action<T> action, Func<T, bool> filter)
-                  {
-                        Action = action;
-                        Filter = filter;
-                  }
-            }
-
-            internal static void SubscribeFiltered(Action<T> action, Func<T, bool> filter)
-            {
-                  if (!_filteredSubscribers.Exists(sub => sub.Action == action))
-                  {
-                        _filteredSubscribers.Add(new Subscriber(action, filter));
-                  }
-            }
-
-            internal static void UnsubscribeFiltered(Action<T> action)
-            {
-                  _filteredSubscribers.RemoveAll(sub => sub.Action == action);
-            }
-
-            [MethodImpl(MethodImplOptions.AggressiveInlining)]
-            internal static void InvokeFiltered(T eventData)
-            {
-                  foreach (Subscriber sub in _filteredSubscribers.ToArray())
-                  {
-                        if (sub.Filter(eventData))
-                        {
-                              sub.Action(eventData);
-                        }
-                  }
-            }
 
             [MethodImpl(MethodImplOptions.AggressiveInlining)]
             internal static void Invoke(T eventData)
